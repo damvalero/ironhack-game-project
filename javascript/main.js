eaa25ca56749a29ddf003ff947e21d54f6b65e38
Daniel Mendes Valero <dmendesvalero@gmail.com>
2019-08-30 14:45:24 +0100

    updated the starting image of the canvas

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,19 @@
 const $canvas = document.querySelector('canvas');
 const $button = document.querySelector('button');
 const context = $canvas.getContext('2d');
+const image = new Image();
+image.src= './images/space1.jpg';
 
 const game = new Game($canvas);
 
-context.save()
-context.fillStyle='black'
-context.fillRect(0,0,$canvas.width,$canvas.height)
-context.restore()
+//makesto charge the page to load the image
+image.addEventListener('load',()=>{
+    context.save()
+    context.fillStyle='red'
+    context.fillRect(0,0,$canvas.width,$canvas.height)
+    context.drawImage(image, 0, 0, $canvas.width, $canvas.height);
+    context.restore()
+})
 
 
 $button.addEventListener('click', () => {
